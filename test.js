f30562c56d75dac5871aa54b7c59e1da3b90c3fd
Dylan Tan <dylantanjl45@hotmail.com>
2023-05-22 22:14:04 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+const myObj = {
+    prop1: 'first value',
+    prop2: 20
+}
+
+const myArray = [40, 200 ,2]
+
+const result = myObj.prop2 === (myArray[0] / myArray[2])
+
+console.log(result)
+
---
+++ test.py
@@ -0,0 +1,7 @@
+print("Your Mom")
+i = 0
+while (i < 50):
+    print("Your Mom Die")
+    i += 1
+
+
